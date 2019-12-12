f779c75160410a8131ce37211e6f6482e8140913
Jean Toledo <jeanctoledo@gmail.com>
2019-12-12 17:25:27 -0300

    setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Label from './components/Label';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Label>Hello world</Label>
+    );
+  }
+}
+
+export default App;
