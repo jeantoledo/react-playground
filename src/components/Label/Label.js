2df13774557251b97f71917fde72cdf2f3534479
Daniel Baptista Dias <danielbpdias@gmail.com>
2019-12-12 23:38:15 -0300

    Added property component

--- src/components/Label/Label.js
+++ src/components/Label/Label.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Label = ({ children }) => (
     <label>{children}</label>
 );
+
+Label.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired
+}
 
 export default Label;
