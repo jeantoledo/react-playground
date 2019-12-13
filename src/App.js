043d4a3482d54e6c9626467908a838c9c317d1fa
Daniel Baptista Dias <danielbpdias@gmail.com>
2019-12-13 00:10:46 -0300

    Moving state control to App.state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import SplitPane from 'react-split-pane';
 
 import Backend from 'react-dnd-html5-backend';
@@ -12,45 +12,28 @@ import Preview from './components/Preview';
 import ComponentList from './components/ComponentList'
 import ComponentProperties from './components/ComponentProperties'
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      currentProperties: [ { name: 'Título', value: 'A volta' }, { name: 'Subtítulo', value: 'dos que não foram' } ]
-    }
-  }
+const App = props => {
+  const { properties, onPropertyChanged } = props
 
-  handlePropertyChanged(changedProperty) {
-    const currentProperties = this.state.currentProperties
-    const [ property ] = currentProperties.filter(prop => prop.name === changedProperty.name)
-    property.value = changedProperty.value
-
-    this.setState(Object.assign(this.state, { currentProperties }))
-  }
-
-  render() {
-    const properties = this.state.currentProperties
-
-    return (
-      <DndProvider backend={Backend}>
-        <SplitPane split="vertical" minSize={50} defaultSize={300}>
-          <div>
-            <ComponentList />
-          </div>
-          <div>
-            <SplitPane split="vertical" minSize={50} defaultSize={300}>
-              <div>
-                <Preview />
-              </div>
-              <div>
-                <ComponentProperties properties={properties} onPropertyChanged={this.handlePropertyChanged.bind(this)} />
-              </div>
-            </SplitPane>
-          </div>
-        </SplitPane>
-      </DndProvider>
-    );
-  }
+  return (
+    <DndProvider backend={Backend}>
+      <SplitPane split="vertical" minSize={50} defaultSize={300}>
+        <div>
+          <ComponentList />
+        </div>
+        <div>
+          <SplitPane split="vertical" minSize={50} defaultSize={300}>
+            <div>
+              <Preview />
+            </div>
+            <div>
+              <ComponentProperties properties={properties} onPropertyChanged={onPropertyChanged} />
+            </div>
+          </SplitPane>
+        </div>
+      </SplitPane>
+    </DndProvider>
+  )
 }
 
 export default App;
